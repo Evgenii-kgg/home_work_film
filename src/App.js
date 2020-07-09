0488e852a376a827037ffe96921f5108afd9b2e9
Солодов Евгений Александрович <evgenii.kgg05@gmail.com>
2020-07-09 19:03:24 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import super1 from "./img/super1.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Stars from "./Stars";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  // change code below this line
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  decrement() {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  }
+
+  reset() {
+    this.setState({
+      count: 0,
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div style={{ top: "50%", left: "50%" }}>
+          <img src={super1} style={{ height: "70px" }} />
+          <h1>фильм: Supermen</h1>
+          <p>Рейтинг фильма:</p>
+            <Stars count={this.state.count} />
+          <div>
+            <button className="inc" onClick={(e) => this.increment(e)}>
+              Increment!
+            </button>
+            <button className="dec" onClick={(e) => this.decrement(e)}>
+              Decrement!
+            </button>
+            <button className="reset" onClick={(e) => this.reset(e)}>
+              Reset
+            </button>
+            <h1>Current Count: {this.state.count}</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
